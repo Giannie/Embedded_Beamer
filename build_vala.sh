db3fadf45a2185a951921b4d236d95829769a03d
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2014-04-03 17:53:29 +0100

    first commit

--- build_vala.sh
+++ build_vala.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-wget http://ftp.gnome.org/pub/gnome/sources/vala/0.20/vala-0.20.1.tar.xz
+if [ ! -e vala-0.20.1.tar.xz ]; then
+    wget http://ftp.gnome.org/pub/gnome/sources/vala/0.20/vala-0.20.1.tar.xz
+fi
 tar xJf vala-0.20.1.tar.xz
 
 cd vala-0.20.1
