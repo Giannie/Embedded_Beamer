3d69a2cc6a5921e3560dd1dc6c01b4db24f47a2d
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2014-04-03 17:38:08 +0100

    first commit

--- dep.sh
+++ dep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install libgstreamer0.10-dev libgee-dev valac-0.20 libgstreamer-plugins-base0.10-dev librsvg2-dev libpoppler-glib-dev libgtk2.0-dev git build-essential libav-tools
+sudo apt-get install libgstreamer0.10-dev libgee-dev valac-0.20 libgstreamer-plugins-base0.10-dev librsvg2-dev libpoppler-glib-dev libgtk2.0-dev build-essential libav-tools
--- dep_old.sh
+++ dep_old.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install libgstreamer0.10-dev libgee-dev libgstreamer-plugins-base0.10-dev librsvg2-dev libpoppler-glib-dev libgtk2.0-dev git build-essential libav-tools flex
+sudo apt-get install libgstreamer0.10-dev libgee-dev libgstreamer-plugins-base0.10-dev librsvg2-dev libpoppler-glib-dev libgtk2.0-dev build-essential libav-tools flex
